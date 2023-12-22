310e2acf4437fbe94349b5ca2f34d7c8b74787c8
co1dsimens <74902928+co1dsimens@users.noreply.github.com>
2023-12-22 18:03:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.height = "100%";
+  document.getElementById("mySidenav").style.paddingTop = "60px";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.paddingTop = "0px";
+  document.getElementById("mySidenav").style.height = "0";
+}
